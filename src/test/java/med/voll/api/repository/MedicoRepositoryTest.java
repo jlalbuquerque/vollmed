ce5df2590bbba7448d8c45c25684e7e435c793f2
jlalbuquerque <joaolca.2008@gmail.com>
2024-05-06 16:34:33 -0300

    Criado MedicoRepositoryTest para testar criação de objeto

---
+++ src/test/java/med/voll/api/repository/MedicoRepositoryTest.java
@@ -0,0 +1,54 @@
+package med.voll.api.repository;
+
+import med.voll.api.model.DadosEndereco;
+import med.voll.api.model.Especialidade;
+import med.voll.api.model.medico.DadosCadastroMedico;
+import med.voll.api.model.medico.Medico;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Optional;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
+public class MedicoRepositoryTest {
+    private TestEntityManager entityManager;
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    public MedicoRepositoryTest(TestEntityManager entityManager, MedicoRepository medicoRepository) {
+        this.entityManager = entityManager;
+        this.medicoRepository = medicoRepository;
+    }
+
+    @Test
+    public void whenSave_thenFindById() {
+        DadosEndereco dadosEnd = new DadosEndereco(
+                "Rua eu sou demais Ferreira Silva",
+                "Bairro Botafogo Futebol Clube",
+                "76864909",
+                "Rio de Janeiro",
+                "RJ",
+                null,
+                "43"
+        );
+        DadosCadastroMedico dados = new DadosCadastroMedico(
+                "Pedro Santos",
+                "pedro.2004@gmail.com",
+                "123457",
+                Especialidade.CARDIOLOGIA,
+                dadosEnd
+        );
+
+        Medico medico = new Medico(dados);
+        entityManager.persistAndFlush(medico);
+
+        Optional<Medico> medEncontrado = medicoRepository.findById(medico.getId());
+
+        Assertions.assertThat(medEncontrado.isPresent()).isEqualTo(true);
+    }
+}
